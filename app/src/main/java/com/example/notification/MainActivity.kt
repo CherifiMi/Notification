1ebd7c3878cd399d32a3f0612ac31fad34b162c4
CherifiMi <cherifi154949@gmail.com>
2022-10-13 02:57:17 +0100

    ui

--- app/src/main/java/com/example/notification/MainActivity.kt
+++ app/src/main/java/com/example/notification/MainActivity.kt
@@ -3,13 +3,18 @@ package com.example.notification
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.notification.ui.theme.NotificationTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    App()
                 }
             }
         }
@@ -30,14 +35,85 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun App() {
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    NotificationTheme {
-        Greeting("Android")
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier.padding(100.dp)
+    ) {
+        Text(
+            "Notifications in Jetpack Compose",
+            style = MaterialTheme.typography.subtitle1,
+            modifier = Modifier.padding(bottom = 100.dp)
+        )
+
+        // simple notification button
+        Button(onClick = {
+           /* showSimpleNotification(
+                context,
+                channelId,
+                notificationId,
+                "Simple notification",
+                "This is a simple notification with default priority."
+            )*/
+        }, modifier = Modifier.padding(top = 16.dp)) {
+            Text(text = "Simple Notification")
+        }
+
+        // simple notification button with tap action
+        Button(onClick = {
+            /*showSimpleNotificationWithTapAction(
+                context,
+                channelId,
+                notificationId,
+                "Simple notification + Tap action",
+                "This simple notification will open an activity on tap."
+            )*/
+        }, modifier = Modifier.padding(top = 16.dp)) {
+            Text(text = "Simple Notification + Tap Action")
+        }
+
+        // large text notification button
+        Button(onClick = {
+            /*showLargeTextNotification(
+                context,
+                channelId,
+                notificationId + 1,
+                "My Large Text Notification",
+                bigText
+            )*/
+        }, modifier = Modifier.padding(top = 16.dp)) {
+            Text(text = "Large Text Notification")
+        }
+
+        // large text with big icon notification
+        Button(onClick = {
+            /*showLargeTextWithBigIconNotification(
+                context,
+                channelId,
+                notificationId + 2,
+                "Large Text with Big Icon Notification",
+                "This is a large text notification with a big icon on the right.",
+                myBitmap
+            )*/
+        }, modifier = Modifier.padding(top = 16.dp)) {
+            Text(text = "Large Text + Big Icon Notification")
+        }
+
+        // big picture with auto hiding thumbnail notification
+        Button(onClick = {
+            /*showBigPictureWithThumbnailNotification(
+                context,
+                channelId,
+                notificationId + 3,
+                "Big Picture + Avatar Notification",
+                "This is a notification showing a big picture and an auto-hiding avatar.",
+                myBitmap
+            )*/
+        }, modifier = Modifier.padding(top = 16.dp)) {
+            Text(text = "Big Picture + Big Icon Notification")
+        }
     }
+
 }
